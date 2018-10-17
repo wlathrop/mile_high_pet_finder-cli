8d0dca4fc68c32eac01e54377501abcd354b7cf7
William Lathrop <wlathrop10@gmail.com>
2018-10-17 22:23:23 +0000

    first commit

---
+++ lib/mile_high_pet_finder/cli.rb
@@ -0,0 +1,7 @@
+require "mile_high_pet_finder/cli/version"
+
+module MileHighPetFinder
+  module Cli
+    # Your code goes here...
+  end
+end
---
+++ lib/mile_high_pet_finder/cli/version.rb
@@ -0,0 +1,5 @@
+module MileHighPetFinder
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
