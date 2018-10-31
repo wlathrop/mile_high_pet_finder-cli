1c8b8022e1bdec7642e68bc352e4a9069078fa5b
William Lathrop <wlathrop10@gmail.com>
2018-10-31 01:39:55 +0000

    added Pet attr

---
+++ lib/pets.rb
@@ -0,0 +1,33 @@
+class Pets
+  attr_accessor :gender, :name, :breed, :age, :url, :description
+
+  def initialize(attributes)
+    attributes.each {|k, v| self.send(("#{k}="), v)}
+    self.class.all << self unless self.class.all.any? {|pet| pet.url == self.url}
+  end
+
+  def self.create_new(pet_array)
+    pet_array.each do |pet_hash|
+      self.new(pet_hash)
+    end
+  end
+
+  def add_attributes(details_hash)
+    details_hash.each do |k, v|
+      self.send(("#{k}="), v)
+    end
+    check_attributes_for_nil
+    self
+  end
+
+  def check_attributes_for_nil
+    attributes = ["gender", "name", "breed", "age", "url" "description"]
+    empty = [" ", "", nil]
+    attributes.each do |var|
+      if empty.any? { |e| self.send("#{var}") == e}
+        self.send(("#{var}="), "N/A")
+      end
+    end
+  end
+
+end
